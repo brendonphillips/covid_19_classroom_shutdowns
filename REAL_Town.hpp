2e2dabd7e24c17816f088e17343a623af749b74f
Brendon Phillips <b2philli@uwaterloo.ca>
2020-09-03 15:47:33 -0400

    Update REAL_Town.hpp

--- REAL_Town.hpp
+++ REAL_Town.hpp
@@ -3,7 +3,7 @@
 
 /*
 	I put *lots* of assertions in the code to make sure I didn't misspell or misroute anything.
-	Turn all the asserts off when gathering data. I reckon it's ~20 times faster without the assertions.
+	Turn all the asserts off (#define NDEBUG in REAL_Town.hpp) when gathering data. I reckon it's ~20 times faster without the assertions.
 */
 #define NDEBUG
 
